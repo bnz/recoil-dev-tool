ddeb5fd94868e326f25ea1287d6f7c308c9aa91b
bonez <cheekbonez@gmail.com>
2023-09-20 16:20:41 +0300

    ...

--- src/DebugObserver.tsx
+++ src/DebugObserver.tsx
@@ -1,11 +1,25 @@
 import { useRecoilCallback, useRecoilSnapshot } from 'recoil'
-import { useEffect, useState } from 'react'
+import { CSSProperties, useEffect, useRef, useState } from 'react'
 import cx from "classnames"
-import CloseIcon from "./assets/close.svg"
-import CloseWhiteIcon from "./assets/close-white.svg"
 import { RenderValue } from "./RenderValue"
 import { themeKey, useTheme } from "./useTheme"
 import { RenderKey } from "./RenderKey"
+import { KeyboardActions } from "./KeyboardActions"
+
+function useBgImage(name: string, asIcon = true): CSSProperties {
+    const isDark = useTheme(true)
+    const url = require(`./assets/${name}${isDark ? "-white" : ""}.svg`)
+    return {
+        backgroundImage: `url(${url})`,
+        ...(asIcon ? {
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "20px 20px",
+            width: 6 * 4,
+            height: 6 * 4,
+        } : {}),
+    }
+}
 
 export function DebugObserver() {
     const snapshot = useRecoilSnapshot()
@@ -14,7 +28,7 @@ export function DebugObserver() {
     const [debugVisible, setDebugVisible] = useState(true)
     const [settingsVisible, setSettingsVisible] = useState(false)
 
-    const isDark = useTheme(true)
+    const [favoriteOnly, setFavoriteOnly] = useState(false)
 
     const onClick = useRecoilCallback(({ snapshot }) => async () => {
         const res: Record<any, { type: any, value: any }> = {}
@@ -66,23 +80,69 @@ export function DebugObserver() {
         return previousValue
     }, [[], [], []] as [string[], string[], string[]])
 
-    console.log({ atomFamilies, atomFamiliesKeys })
+    // console.log({ atomFamilies, atomFamiliesKeys })
+
+    const ref = useRef<HTMLDivElement | null>(null)
+    const inputRef = useRef<HTMLInputElement | null>(null)
+
+    const closeButtonStyles = useBgImage("close")
 
     return (
-        <div className={cx(
+        <div tabIndex={0} ref={ref} className={cx(
             !settingsVisible && "border border-[var(--line-color)]",
             "fixed bottom-10 left-1",
             "rounded-xl shadow-2xl max-w-fit min-w-[50%] z-40 bg-[var(--background-color)]",
             "overflow-hidden",
+            "focus-within:outline focus-within:outline-4 focus-within:outline-[var(--line-color-focus)] focus-within:outline-offset-8",
         )}>
-            <header className="border-b border-[var(--line-color)] flex">
-                <div className="flex-1 italic">recoil.js?DevTools</div>
-                <button
-                    className="text-gray-500 border-0 border-l border-l-[var(--line-color)] px-2"
-                    onClick={() => setSettingsVisible(true)}
+            <header className="border-b border-[var(--line-color)] flex items-center">
+                <div
+                    className="italic flex items-center px-1 bg-[linear-gradient(to_right,orange,violet)]"
+                    // @ts-ignore
+                    style={{ "-webkit-background-clip": "text", "-webkit-text-fill-color": "transparent" }}
                 >
-                    settings
-                </button>
+                    recoil.js?DevTools ツ
+                </div>
+                <div
+                    className="ml-3 bg-no-repeat bg-[4px_center] bg-[length:20px_20px]"
+                    style={useBgImage("search", false)}
+                >
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        placeholder="Filter (press F key)"
+                        className={cx(
+                            "pr-2 pl-6 my-1 rounded border border-[var(--line-color)] bg-transparent box-content",
+                            "outline-none",
+                            "focus:border-[var(--line-color-focus)]",
+                            "focus:placeholder:text-[var(--text-color-disabled)]",
+                        )}
+                    />
+                    {ref.current !== null && inputRef.current && (
+                        <KeyboardActions target={ref.current} actions={{
+                            Escape() {
+                                if (inputRef.current) {
+                                    inputRef.current.value = ""
+                                }
+                            },
+                            KeyF() {
+                                if (inputRef.current && document.activeElement !== inputRef.current) {
+                                    inputRef.current.focus()
+                                }
+                            },
+                        }} />
+                    )}
+                </div>
+                <button
+                    style={useBgImage(favoriteOnly ? "star-fill" : "star")}
+                    onClick={() => setFavoriteOnly((prev) => !prev)}
+                />
+                <button
+                    // className="px-2"
+                    onClick={() => setSettingsVisible(true)}
+                    style={useBgImage("settings")}
+                    title="Settings"
+                />
                 <button
                     className="text-gray-500 border-0 border-l border-l-[var(--line-color)] px-2"
                     onClick={() => setDebugVisible((prev) => !prev)}
@@ -125,25 +185,36 @@ export function DebugObserver() {
                 })}
             </pre>
             {settingsVisible && (
-                <div className="absolute inset-0 bg-black/30 backdrop-blur-sm">
-                    <div className="absolute inset-5 bg-[var(--background-color)] rounded-xl shadow">
-                        <header className="flex border-b border-b-[var(--line-color)] p-2">
-                            <div className="flex-1">
-                                Settings
+                <>
+                    <KeyboardActions actions={{
+                        Escape: () => {
+                            setSettingsVisible(false)
+                            ref.current?.focus()
+                        },
+                    }} />
+                    <div className="absolute inset-0 bg-black/30 backdrop-blur-sm">
+                        <div className="absolute inset-5 bg-[var(--background-color)] rounded-xl shadow">
+                            <header className="flex border-b border-b-[var(--line-color)] p-2">
+                                <div className="flex-1">
+                                    Settings
+                                </div>
+                                <button
+                                    className="w-6 h-6"
+                                    style={{ ...closeButtonStyles }}
+                                    onClick={() => {
+                                        setSettingsVisible(false)
+                                        ref.current?.focus()
+                                    }}
+                                />
+                            </header>
+                            <div className="p-2">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae consequatur cum
+                                distinctio excepturi facilis fugiat harum id inventore ipsam magnam maiores nesciunt
+                                nisi quibusdam, quod rem, repellendus vitae voluptatem.
                             </div>
-                            <button
-                                className="bg-no-repeat w-6 h-6"
-                                style={{ backgroundImage: `url(${isDark ? CloseWhiteIcon : CloseIcon})` }}
-                                onClick={() => setSettingsVisible(false)}
-                            />
-                        </header>
-                        <div className="p-2">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae consequatur cum
-                            distinctio excepturi facilis fugiat harum id inventore ipsam magnam maiores nesciunt nisi
-                            quibusdam, quod rem, repellendus vitae voluptatem.
                         </div>
                     </div>
-                </div>
+                </>
             )}
         </div>
     )
